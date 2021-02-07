7a6c14d18535ab4bb008bf320443f36f292dad25
Jo Ey Chong <37789769+Joey-Chong@users.noreply.github.com>
2021-02-07 21:09:52 +0800

    null detection added

--- Server/ServerInit.java
+++ Server/ServerInit.java
@@ -4,7 +4,13 @@ import java.io.*;
 
 public class ServerInit {
     //if possible, change the way IOException is thrown or caught
-    public static void start() throws IOException {
+
+    public ServerInit() throws IOException {
+        System.out.println("Server started");
+        start();
+    }
+
+    public void start() throws IOException {
         ServerSocket socket = new ServerSocket(8080);   //this is default
         Socket client = null;
 
@@ -14,14 +20,19 @@ public class ServerInit {
         }
     }
 
-    public static void readRequest(Socket client) throws IOException{
+    public void readRequest(Socket client) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
         while(true) {
             String line = reader.readLine();
             //debugger
             System.out.println(">" + line);
+            //postman null debugging needed
+            if(line == null) {
+                break;
+            }
             //indicate end of request or header, add body later
             if(line.equals("\r\n")) {
+//                System.out.println("line break");
                 break;
             }
         }
