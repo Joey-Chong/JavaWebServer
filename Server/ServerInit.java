a00aff7e2b6f2a44bffeb12d40153312d57c2ac6
Jo Ey Chong <37789769+Joey-Chong@users.noreply.github.com>
2021-02-13 19:47:05 +0800

    Moved readRequest method to new class ParseHttpRequest for encapsulation

--- Server/ServerInit.java
+++ Server/ServerInit.java
@@ -1,33 +1,45 @@
 package Server;
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
 
 public class ServerInit {
+    ParseHttpRequest parser;
+    HttpResponse responser;
+
     //if possible, change the way IOException is thrown or caught
-    public static void start() throws IOException {
+    public ServerInit() throws IOException {
+        System.out.println("Server started");
+        System.out.println("Preparing Mime Type");
+        ContentTypeInit contentTypes = new ContentTypeInit();
+        System.out.println("Starting Server");
+        System.out.println("------------------------------------------");
+        start();
+    }
+
+    private void start() throws IOException {
         ServerSocket socket = new ServerSocket(8080);   //this is default
         Socket client = null;
 
         while(true) {
             client = socket.accept();
-            readRequest(client);
+            parser = new ParseHttpRequest(client);
+            responser = new HttpResponse(parser);
+//            readRequest(client);
+//            sendResponse(client);
+            client.close();
         }
     }
 
-    public static void readRequest(Socket client) throws IOException{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        while(true) {
-            String line = reader.readLine();
-            //debugger
-            System.out.println(">" + line);
-            //indicate end of request or header, add body later
-            if(line.equals("\\r\\n")) {
-                //debugger
-                System.out.println("End of request");
-                break;
-            }
-        }
-        //debugger
-        System.out.println("Request completed");
+    //will be moved, this is just a prototype
+    private void sendResponse(Socket client) throws IOException {
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        System.out.println("Sending response");
+        out.print("HTTP/1.1 404 Not found\r\n");
+//        out.print("HTTP/1.1 401 Unauthorized\r\n");
+//        out.print("WWW-Authenticate: Basic\r\n");
+        out.print("\r\n");
+        out.flush();
+        System.out.println("Sent");
     }
 }
