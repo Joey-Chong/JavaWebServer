da531d83a74a2e6e766d32b0d4376573176de0e0
Jo Ey Chong <37789769+Joey-Chong@users.noreply.github.com>
2021-02-15 16:01:52 +0800

    Added more parsing on headers and body

--- Server/ParseHttpRequest.java
+++ Server/ParseHttpRequest.java
@@ -20,14 +20,31 @@ public class ParseHttpRequest {
         BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
         String line = null;
         System.out.println("------- Header -------");
+
         System.out.println("------- Method -------");
         line = reader.readLine();
-        System.out.println(line);
-        //below add parsing http method
+        System.out.println(">" + line);
+        String methodLine[] = line.split(" ");
+        if(methodLine.length != 3) {
+            System.out.println("Invalid method line");
+            System.exit(1);
+        }
+        method = methodLine[0];
+        identifier = methodLine[1];
+        version = methodLine[2];
+        version.replace("\\r\\n", "");
+//        for(String m: methodLine) {
+//            System.out.println(m);
+//        }
 
+        String[] headerLine;
         System.out.println("------- Rest of Headers -------");
         while((line = reader.readLine()).length() != 0) {
             System.out.println(">" + line);
+
+            headerLine = line.split(": ");
+            headerMap.put(headerLine[0], headerLine[1]);
+
             //if no Content-Length is given from client or server, it indicates there is no body
             if((line == null) || line.equals("\\r\\n")) {
                 //debugger
@@ -35,15 +52,21 @@ public class ParseHttpRequest {
                 break;
             }
         }
-        String body = null;
-        System.out.println("------- Body -------");
-        while((body = reader.readLine()) != null) {
-            System.out.println(">" + body);
-            if(body.equals("\\r\\n")) {
-                break;
+
+        if(headerMap.containsKey("Content-Type") && headerMap.containsKey("Content-Length")) {
+            body = null;
+            System.out.println("------- Body -------");
+            while ((body = reader.readLine()) != null) {
+                System.out.println(">" + body);
+                if (body.equals("\\r\\n")) {
+                    break;
+                }
             }
+            System.out.println("------- End of Body -------");
         }
-        System.out.println("------- End of body -------");
+        else {
+            System.out.println("------- No Body Present -------");
+        }
     }
 
     public String getMethod() {
